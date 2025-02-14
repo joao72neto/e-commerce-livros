787a884d6fa68dc395bdfd688b87025d97f54843
joao_s_neto <joao72neto@gmail.com>
2025-02-14 18:11:10 -0300

    Deixe o botao funcional

--- prototipo-ia/script.js
+++ prototipo-ia/script.js
@@ -1,28 +1,52 @@
+//Elements
 let button = document.querySelector('button');
 let screen = document.querySelector('.screen');
+let chat_button = document.querySelector('.chat-button');
 
-button.addEventListener('click', () => {
+//Functions
+let click_msg = () => {
 
-    let input = document.querySelector('input').value;
+    button.addEventListener('click', () => {
 
-    if(input){
+        let input = document.querySelector('input').value;
 
-        screen.innerHTML += `<p 
-                                style="margin: 30px 0px 0px 20px; 
-                                border-radius: 20px 0px 0px 20px;"
-                            >${input}</p>`;
+        if(input){
 
-        screen.innerHTML += `<p 
-                                style="margin-top: 30px;"
-                            >Parece que você está muito interessado em ${input}</p>`;
-        return;
-    }
+            screen.innerHTML += `<p 
+                                    style="margin: 30px 0px 0px 20px; 
+                                    border-radius: 20px 0px 0px 20px;"
+                                >${input}</p>`;
 
-    alert('Digite algo antes de enviar');
-    
-});
+            screen.innerHTML += `<p 
+                                    style="margin-top: 30px;"
+                                >Parece que você está muito interessado em ${input}</p>`;
+            return;
+        }
+
+        alert('Digite algo antes de enviar');
+        
+    });
+
+}
+
+let click_ia = () => {
+    chat_button.addEventListener('click', () => {
+        let chat = document.querySelector('.chat');
+
+        if(chat.style.display != 'none'){
+            chat.style.display = 'none';
+            return;
+        }
+
+        chat.style.display = 'flex';
+ 
+    });
+}
 
 
+//Calling all the functions
+click_msg();
+click_ia();
 
 
 
