89b2f3fe269f0145a067be88bfdc34fe3b755cc1
joao_s_neto <joao72neto@gmail.com>
2025-03-03 12:22:03 -0300

    Atualize os nomes das tabelas do banco

--- modelo-bd/ddl-banco/e-commerce-livros.sql
+++ modelo-bd/ddl-banco/e-commerce-livros.sql
@@ -15,9 +15,9 @@ CREATE SCHEMA IF NOT EXISTS `e_commerce_books` DEFAULT CHARACTER SET utf8 ;
 USE `e_commerce_books` ;
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Clientes`
+-- Table `e_commerce_books`.`clientes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Clientes` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`clientes` (
   `clt_id` INT NOT NULL AUTO_INCREMENT,
   `clt_nome` VARCHAR(45) NOT NULL,
   `clt_genero` CHAR(1) NOT NULL,
@@ -35,9 +35,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Enderecos`
+-- Table `e_commerce_books`.`enderecos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Enderecos` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`enderecos` (
   `end_id` INT NOT NULL AUTO_INCREMENT,
   `end_clt_id` INT NOT NULL,
   `end_nome` VARCHAR(45) NOT NULL,
@@ -54,16 +54,16 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Enderecos` (
   INDEX `fk_end_clt_idx` (`end_clt_id` ASC) VISIBLE,
   CONSTRAINT `fk_end_clt`
     FOREIGN KEY (`end_clt_id`)
-    REFERENCES `e_commerce_books`.`Clientes` (`clt_id`)
+    REFERENCES `e_commerce_books`.`clientes` (`clt_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`GrupoPrecificacao`
+-- Table `e_commerce_books`.`grupo_precificacao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`GrupoPrecificacao` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`grupo_precificacao` (
   `gpp_id` INT NOT NULL AUTO_INCREMENT,
   `gpp_nome` VARCHAR(45) NOT NULL,
   `gpp_margemLucro` DECIMAL(5,2) NOT NULL,
@@ -72,9 +72,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Autores`
+-- Table `e_commerce_books`.`autores`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Autores` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`autores` (
   `aut_id` INT NOT NULL AUTO_INCREMENT,
   `aut_nome` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`aut_id`))
@@ -82,9 +82,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Editora`
+-- Table `e_commerce_books`.`editora`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Editora` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`editora` (
   `edt_id` INT NOT NULL AUTO_INCREMENT,
   `edt_nome` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`edt_id`))
@@ -92,9 +92,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Livros`
+-- Table `e_commerce_books`.`livros`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Livros` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`livros` (
   `lvr_id` INT NOT NULL AUTO_INCREMENT,
   `lvr_gpp_id` INT NOT NULL,
   `lvr_edt_id` INT NOT NULL,
@@ -117,26 +117,26 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Livros` (
   INDEX `fk_lvr_edt_idx` (`lvr_edt_id` ASC) INVISIBLE,
   CONSTRAINT `fk_lvr_gpp`
     FOREIGN KEY (`lvr_gpp_id`)
-    REFERENCES `e_commerce_books`.`GrupoPrecificacao` (`gpp_id`)
+    REFERENCES `e_commerce_books`.`grupo_precificacao` (`gpp_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_lvr_aut`
     FOREIGN KEY (`lvr_aut_id`)
-    REFERENCES `e_commerce_books`.`Autores` (`aut_id`)
+    REFERENCES `e_commerce_books`.`autores` (`aut_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_lvr_edt`
     FOREIGN KEY (`lvr_edt_id`)
-    REFERENCES `e_commerce_books`.`Editora` (`edt_id`)
+    REFERENCES `e_commerce_books`.`editora` (`edt_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Categoria`
+-- Table `e_commerce_books`.`categoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Categoria` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`categoria` (
   `cat_id` INT NOT NULL AUTO_INCREMENT,
   `cat_nome` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`cat_id`))
@@ -144,9 +144,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Cartoes`
+-- Table `e_commerce_books`.`cartoes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Cartoes` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`cartoes` (
   `car_id` INT NOT NULL AUTO_INCREMENT,
   `car_clt_id` INT NOT NULL,
   `car_nome` VARCHAR(45) NOT NULL,
@@ -158,16 +158,16 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Cartoes` (
   INDEX `fk_car_clt_idx` (`car_clt_id` ASC) VISIBLE,
   CONSTRAINT `fk_car_clt`
     FOREIGN KEY (`car_clt_id`)
-    REFERENCES `e_commerce_books`.`Clientes` (`clt_id`)
+    REFERENCES `e_commerce_books`.`clientes` (`clt_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Fornecedor`
+-- Table `e_commerce_books`.`fornecedor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Fornecedor` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`fornecedor` (
   `for_id` INT NOT NULL AUTO_INCREMENT,
   `for_nome` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`for_id`))
@@ -175,9 +175,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Estoque`
+-- Table `e_commerce_books`.`estoque`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Estoque` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`estoque` (
   `est_id` INT NOT NULL AUTO_INCREMENT,
   `est_for_id` INT NOT NULL,
   `est_lvr_id` INT NOT NULL,
@@ -189,21 +189,21 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Estoque` (
   INDEX `fk_est_lvr_idx` (`est_lvr_id` ASC) VISIBLE,
   CONSTRAINT `fk_est_for`
     FOREIGN KEY (`est_for_id`)
-    REFERENCES `e_commerce_books`.`Fornecedor` (`for_id`)
+    REFERENCES `e_commerce_books`.`fornecedor` (`for_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_est_lvr`
     FOREIGN KEY (`est_lvr_id`)
-    REFERENCES `e_commerce_books`.`Livros` (`lvr_id`)
+    REFERENCES `e_commerce_books`.`livros` (`lvr_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Vendas`
+-- Table `e_commerce_books`.`vendas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Vendas` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`vendas` (
   `vnd_id` INT NOT NULL AUTO_INCREMENT,
   `vnd_clt_id` INT NOT NULL,
   `vnd_lvr_id` INT NOT NULL,
@@ -216,21 +216,21 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Vendas` (
   INDEX `fk_vnd_clt_idx` (`vnd_clt_id` ASC) VISIBLE,
   CONSTRAINT `fk_vnd_clt`
     FOREIGN KEY (`vnd_clt_id`)
-    REFERENCES `e_commerce_books`.`Clientes` (`clt_id`)
+    REFERENCES `e_commerce_books`.`clientes` (`clt_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_vnd_lvr`
     FOREIGN KEY (`vnd_lvr_id`)
-    REFERENCES `e_commerce_books`.`Livros` (`lvr_id`)
+    REFERENCES `e_commerce_books`.`livros` (`lvr_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Carrinho`
+-- Table `e_commerce_books`.`carrinho`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Carrinho` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`carrinho` (
   `crr_id` INT NOT NULL AUTO_INCREMENT,
   `crr_clt_id` INT NOT NULL,
   `crr_lvr_id` INT NOT NULL,
@@ -242,21 +242,21 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Carrinho` (
   INDEX `fk_crr_clt_idx` (`crr_clt_id` ASC) VISIBLE,
   CONSTRAINT `fk_crr_clt`
     FOREIGN KEY (`crr_clt_id`)
-    REFERENCES `e_commerce_books`.`Clientes` (`clt_id`)
+    REFERENCES `e_commerce_books`.`clientes` (`clt_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_crr_lvr`
     FOREIGN KEY (`crr_lvr_id`)
-    REFERENCES `e_commerce_books`.`Livros` (`lvr_id`)
+    REFERENCES `e_commerce_books`.`livros` (`lvr_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Cupons`
+-- Table `e_commerce_books`.`cupons`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Cupons` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`cupons` (
   `cup_id` INT NOT NULL AUTO_INCREMENT,
   `cup_clt_id` INT NOT NULL,
   `cup_codigo` VARCHAR(45) NOT NULL,
@@ -267,16 +267,16 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Cupons` (
   INDEX `fk_cup_clt_idx` (`cup_clt_id` ASC) VISIBLE,
   CONSTRAINT `fk_cup_clt`
     FOREIGN KEY (`cup_clt_id`)
-    REFERENCES `e_commerce_books`.`Clientes` (`clt_id`)
+    REFERENCES `e_commerce_books`.`clientes` (`clt_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Trocas`
+-- Table `e_commerce_books`.`trocas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Trocas` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`trocas` (
   `trc_id` INT NOT NULL AUTO_INCREMENT,
   `trc_clt_id` INT NOT NULL,
   `trc_lvr_id` INT NOT NULL,
@@ -287,21 +287,21 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Trocas` (
   INDEX `fk_trc_clt_idx` (`trc_clt_id` ASC) VISIBLE,
   CONSTRAINT `fk_trc_clt`
     FOREIGN KEY (`trc_clt_id`)
-    REFERENCES `e_commerce_books`.`Clientes` (`clt_id`)
+    REFERENCES `e_commerce_books`.`clientes` (`clt_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_trc_lvr`
     FOREIGN KEY (`trc_lvr_id`)
-    REFERENCES `e_commerce_books`.`Livros` (`lvr_id`)
+    REFERENCES `e_commerce_books`.`livros` (`lvr_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Pagamento`
+-- Table `e_commerce_books`.`pagamento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Pagamento` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`pagamento` (
   `pag_id` INT NOT NULL AUTO_INCREMENT,
   `pag_vnd_id` INT NOT NULL,
   `pag_metodo` ENUM('cartao_credito', 'cupom_troca', 'cupom_promocional') NOT NULL,
@@ -310,16 +310,16 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Pagamento` (
   INDEX `fk_pag_vnd_idx` (`pag_vnd_id` ASC) VISIBLE,
   CONSTRAINT `fk_pag_vnd`
     FOREIGN KEY (`pag_vnd_id`)
-    REFERENCES `e_commerce_books`.`Vendas` (`vnd_id`)
+    REFERENCES `e_commerce_books`.`vendas` (`vnd_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Transacoes`
+-- Table `e_commerce_books`.`transacoes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Transacoes` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`transacoes` (
   `trs_id` INT NOT NULL AUTO_INCREMENT,
   `trs_clt_id` INT NOT NULL,
   `trs_dataHora` DATETIME NOT NULL,
@@ -329,16 +329,16 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Transacoes` (
   INDEX `fk_trc_clt_idx` (`trs_clt_id` ASC) VISIBLE,
   CONSTRAINT `fk_trs_clt`
     FOREIGN KEY (`trs_clt_id`)
-    REFERENCES `e_commerce_books`.`Clientes` (`clt_id`)
+    REFERENCES `e_commerce_books`.`clientes` (`clt_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`HistoricoVendas`
+-- Table `e_commerce_books`.`historico_vendas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`HistoricoVendas` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`historico_vendas` (
   `hvd_id` INT NOT NULL AUTO_INCREMENT,
   `hvd_lvr_id` INT NOT NULL,
   `hvd_qtd` INT NOT NULL,
@@ -348,16 +348,16 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`HistoricoVendas` (
   INDEX `fk_hvd_lvr_idx` (`hvd_lvr_id` ASC) VISIBLE,
   CONSTRAINT `fk_hvd_lvr`
     FOREIGN KEY (`hvd_lvr_id`)
-    REFERENCES `e_commerce_books`.`Livros` (`lvr_id`)
+    REFERENCES `e_commerce_books`.`livros` (`lvr_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`Log`
+-- Table `e_commerce_books`.`log`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Log` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`log` (
   `log_id` INT NOT NULL AUTO_INCREMENT,
   `log_clt_id` INT NOT NULL,
   `log_dataHora` DATETIME NOT NULL,
@@ -367,16 +367,16 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`Log` (
   INDEX `fk_log_clt_idx` (`log_clt_id` ASC) VISIBLE,
   CONSTRAINT `fk_log_clt`
     FOREIGN KEY (`log_clt_id`)
-    REFERENCES `e_commerce_books`.`Clientes` (`clt_id`)
+    REFERENCES `e_commerce_books`.`clientes` (`clt_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `e_commerce_books`.`livrosCategorias`
+-- Table `e_commerce_books`.`livros_categorias`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e_commerce_books`.`livrosCategorias` (
+CREATE TABLE IF NOT EXISTS `e_commerce_books`.`livros_categorias` (
   `lvc_id` INT NOT NULL AUTO_INCREMENT,
   `lvc_lvr_id` INT NOT NULL,
   `lvc_cat_id` INT NOT NULL,
@@ -385,12 +385,12 @@ CREATE TABLE IF NOT EXISTS `e_commerce_books`.`livrosCategorias` (
   INDEX `fk_lvc_lvr_idx` (`lvc_lvr_id` ASC) VISIBLE,
   CONSTRAINT `fk_lvc_lvr`
     FOREIGN KEY (`lvc_lvr_id`)
-    REFERENCES `e_commerce_books`.`Livros` (`lvr_id`)
+    REFERENCES `e_commerce_books`.`livros` (`lvr_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_lvc_cat`
     FOREIGN KEY (`lvc_cat_id`)
-    REFERENCES `e_commerce_books`.`Categoria` (`cat_id`)
+    REFERENCES `e_commerce_books`.`categoria` (`cat_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
