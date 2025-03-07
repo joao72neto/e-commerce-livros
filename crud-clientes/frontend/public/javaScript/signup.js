e98beb121716d79f0a2af42c1d6260ba7bb71146
joao_s_neto <joao72neto@gmail.com>
2025-03-07 14:37:56 -0300

    Pegue todos os dados do formulario e tente gravar no banco

--- crud-clientes/frontend/public/javaScript/signup.js
+++ crud-clientes/frontend/public/javaScript/signup.js
@@ -1,3 +1,66 @@
+import { signupService } from "/javaScript/service/serviceSignup.js";
+
+//PASSANDO OS DADOS PARA CADASTRO
+document.querySelector('form').addEventListener('submit', async function (event) {
+    
+    event.preventDefault();
+
+    //Pegando o ID do cliente
+    const clt_id = window.location.pathname.split('/').splice(-1)[0];
+    console.log(clt_id);
+
+    //Pegando dados do formulário
+    const formDados = new FormData(event.target);
+    let dados = Object.fromEntries(formDados.entries());
+
+    //Pegando o gênero do cliente
+    const cliente = {
+        clt_nome: dados.nome,
+        clt_genero: dados.genero,
+        clt_dataNasc: dados.data, 
+        clt_cpf: dados.cpf, 
+        clt_telefone: dados.telefone,
+        clt_email: dados.email,
+        clt_senha: dados.senha     
+    };
+
+    const address = {
+        end_clt_id: clt_id,
+        end_nome: dados.logradouro,
+        end_tipoResidencia: dados.tipo_residencia,
+        end_tipoLogradouro: dados.tipo_logradouro,
+        end_logradouro: dados.logradouro,
+        end_numero: dados.numero,
+        end_bairro: dados.bairro,
+        end_cep: dados.cep,
+        end_cidade: dados.cidade,
+        end_estado: dados.estado,
+        end_pais: dados.pais
+
+    }
+
+    const card = {
+        car_clt_id: clt_id,
+        car_nome: dados.nome_cartao,
+        car_numero: dados.numero_cartao,
+        car_bandeira: dados.bandeira_cartao,
+        car_cvv: dados.codigo_seguranca
+    }
+
+    const signupDados = {cliente, address, card}
+
+    const status = await signupService(signupDados);
+
+    if(status === 200){
+        alert('DEU MUITO BOM!');
+        return;
+    }
+
+    alert('deu muito ruim');
+
+})
+
+
 //ADICIONANDO O FUNCIONAMENTO DA CHECKBOX
 document.querySelector('input[type="checkbox"]').addEventListener('click', function(){
     
