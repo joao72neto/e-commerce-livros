b78d4d8b35b3134ed3b41ac9fb70fb671f3b01ce
joao_s_neto <joao72neto@gmail.com>
2025-03-14 18:12:12 -0300

    test: criar teste para chamar a tela de senha corretamente

---
+++ crud-clientes/tests/backend/controller/controllerPassword.test.js
@@ -0,0 +1,17 @@
+const { getPassword } = require("../../../backend/controller/controllerPassword");
+
+
+//Testes
+test('Deve chamar a tela de senha corretamente', () => {
+
+    //Criando um mock do req e res
+    const req = {}
+    const res = {render: jest.fn()}
+
+    //Chamando a função controller para renderizar a tela de senha
+    getPassword(req, res);
+
+    //Validando os dados
+    expect(res.render).toHaveBeenCalledWith('password');
+    
+});
