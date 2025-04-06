22c571f6243fceab9ac06cbc706a49fe03b1e1f0
joao_s_neto <joao72neto@gmail.com>
2025-04-06 13:24:20 -0300

    feat: adicionar rota para cliente logado e não logado

--- crud-clientes/backend/controller/controllerIndex.js
+++ crud-clientes/backend/controller/controllerIndex.js
@@ -3,7 +3,10 @@ const { buscarTodosLivro } = require('../model/modelIndex');
 //Página
 module.exports.getIndex = async (req, res) => {
     const livros = await buscarTodosLivro();
-    res.render('index', {livros: livros});
+    res.render('index/index', {livros: livros});
 };
 
+module.exports.getIndexLogado = (req, res) => {
+    res.render('index/indexLogado');
+};
 
--- crud-clientes/backend/routes/routeIndex.js
+++ crud-clientes/backend/routes/routeIndex.js
@@ -4,5 +4,6 @@ const controllerIndex = require('../controller/controllerIndex');
 
 //Rotas para páginas
 router.get('/', controllerIndex.getIndex);
+router.get('/login/:clt_id', controllerIndex.getIndexLogado);
 
 module.exports = router;
