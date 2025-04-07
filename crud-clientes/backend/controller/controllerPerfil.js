eb6dc0fdaf033657dc3278da7709e7ebf7c27020
joao_s_neto <joao72neto@gmail.com>
2025-04-06 21:56:24 -0300

    feat: carregar todos os dados do cliente no seu perfil

--- crud-clientes/backend/controller/controllerPerfil.js
+++ crud-clientes/backend/controller/controllerPerfil.js
@@ -1,8 +1,18 @@
 const { buscarClienteLogado } = require('../model/clientes/modelClientes');
+const { buscarCartoesClienteId } = require('../model/clientes/modelCard');
+const { buscarEnderecosClienteId } = require('../model/clientes/modelAddress');
+
 
 //Página
 module.exports.getPerfil = async (req, res) => {
     const cliente = await buscarClienteLogado();
+    const cartoes = await buscarCartoesClienteId(cliente[0].clt_id);
+    const enderecos = await buscarEnderecosClienteId(cliente[0].clt_id);
+
     console.log(cliente);
-    res.render('perfil', {cliente: cliente[0]});
+    res.render('perfil', {
+        cliente: cliente[0],
+        cartoes: cartoes,
+        enderecos: enderecos
+    });
 };
