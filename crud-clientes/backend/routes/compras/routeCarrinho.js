e495871b03773d47a2a6a4d30ea338982c874b82
joao_s_neto <joao72neto@gmail.com>
2025-04-08 13:48:36 -0300

    feat: adicionar rotas para manipulação do carrinho

--- crud-clientes/backend/routes/compras/routeCarrinho.js
+++ crud-clientes/backend/routes/compras/routeCarrinho.js
@@ -5,4 +5,13 @@ const controllerCarrinho = require('../../controller/compras/controllerCarrinho'
 //Rotas para páginas
 router.get('/carrinho', controllerCarrinho.getCarrinho);
 
+//Rotas para apis
+router.get('/api/carrinho/:clt_id', controllerCarrinho.getApiCarrinhoClienteId);
+
+//Rotas para deletar dados
+router.delete('/carrinho/delete/:lvr_id', controllerCarrinho.deleteCarrinhoId);
+
+//Rotas para inserção de dados
+router.post('/carrinho/add', controllerCarrinho.postCarrinho);
+
 module.exports = router;
