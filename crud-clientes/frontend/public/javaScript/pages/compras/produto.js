e3118f790e550622f7532d8cb618d92c8047d291
joao_s_neto <joao72neto@gmail.com>
2025-04-08 22:52:00 -0300

    style: ajustar a organização dos livros na tela principal

--- crud-clientes/frontend/public/javaScript/pages/compras/produto.js
+++ crud-clientes/frontend/public/javaScript/pages/compras/produto.js
@@ -1,20 +1,41 @@
 import { buscarClienteLogadoService } from "../../service/clientes/serviceClientes.js"
 import { adicionarCarrinhoService } from "../../service/compras/serviceCarrinho.js"
 
+
+
 //Aumentado a quatidado do item
 document.querySelector('.aumentar').addEventListener('click', function(){
+    
+    //Pegando o preço
+    let preco = Number(document.querySelector('.preco').textContent.split('R$')[1].replace(',', '.'));
+    
     const contador = document.querySelector('#contador');
     contador.textContent = Number(contador.textContent) + 1;
+    preco += preco;
+
+    console.log(preco);
 });
 
 
 //Diminuindo a quantidade do item
 document.querySelector('.diminuir').addEventListener('click', function(){
 
+    //Pegando o preço
+    let preco = Number(document.querySelector('.preco').textContent.split('R$')[1].replace(',', '.'));
+
     const contador = document.querySelector('#contador');
+
+    if(Number(contador.textContent) === 1){
+        var precoBase = preco; 
+    }
+       
+
     if(Number(contador.textContent) > 1){
         contador.textContent = Number(contador.textContent) - 1;
+        preco -= precoBase
     }
+
+    console.log(preco);
 });
 
 //Adicionando item ao carrinho
