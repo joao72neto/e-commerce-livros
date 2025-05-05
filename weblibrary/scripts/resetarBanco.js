9f9f67039972f3727c223fa6c11225565c95da7a
João Neto <joao72neto@gmail.com>
2025-05-05 01:00:09 -0300

    fix: terminar processo ao resetar o banco

--- weblibrary/scripts/resetarBanco.js
+++ weblibrary/scripts/resetarBanco.js
@@ -5,7 +5,9 @@ const { resetarPovoarBanco } = require('../backend/model/database/modelReset');
     try{
         await resetarPovoarBanco();
         console.log('Banco de dados resetado e povoado!');
+        process.exit(0);
     }catch(err){
         console.error(`Erro ao resetar/povoar banco: ${err}`);
+        process.exit(1);
     }
 })();
