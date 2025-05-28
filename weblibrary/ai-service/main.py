3f264f26239b827e05e01937e9673305196ae471
joao_s_neto <joao72neto@gmail.com>
2025-05-28 18:40:10 -0300

    feat/fix: melhorar o promp e fazer com que o histórico de conversa seja mantido

--- weblibrary/ai-service/main.py
+++ weblibrary/ai-service/main.py
@@ -26,22 +26,42 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+#Criando o index faiss filtrado
+livros = lvr.buscar_todos_livros()
+text = [livro['lvr_titulo'] + " com custo de " + livro['lvr_preco'] + " " + livro['lvr_sinopse'] for livro in livros]
+books_embedding = ebd.gerar_embedding(text)
+index = fid.criar_index_faiss(books_embedding)
+    
+#Configurando a IA
+load_dotenv()
+api_key = os.getenv('GOOGLE_API_KEY')
+
+#Configurando a minha api_key
+client = genai.Client(api_key=api_key)
+
+#Defininfo o meumodelo e o conteúdo que eu vou mostrar
+model = 'gemini-2.0-flash'
+    
+#Preparando a intrução para a IA
+system_instruction = (
+    "Você é um assistente pessoal de um e-commerce de livros chamado weblibrary e deve agir como tal, respondendo perguntas relacionadas a livros. Sue nome não é mais Gemini, você é IA do e-commerce WebLibrary. Além disso você deve agir de forma amígavel e persuasiva ao mesmo tempo, a fim de fazer com que o vendedor se interesse em comprar determinado tipo de livro \n------------------------------------\nUse as informações que serão enviadas via chat precedidas do seguinte texto 'Dados a serem considerados na conversa: ', você não responde nada sobre esses dados a não ser que o cliente requisite, eles vão servir de base para as respostas, o cliente pergunta alguma coisa sobre um livro, você olha os dados e com base neles responde o cliente. As respostar precisam ser respondidas de forma concisa, evite responder algum muito grande na primeira msg que o cliente mandar, se for um oi, ola, responda sucintamente. Novos dados virão conforme a conversa é desenrolada, você apenas considera eles conforme a conversa vai seguindo, nunca fuja dos dados, qualquer desvio não deixaria claro o que o e-commerce de fato tem e não tem para o cliente:\n\n"
+)
+       
+#Configurando o conteúdo que deve ser gerado
+chat_config = types.GenerateContentConfig(
+    system_instruction = system_instruction
+)
+
+#Criando um chat para conversar com a IA
+chat = client.chats.create(model=model, config=chat_config)
+        
 #Criando um modelo para receber a msg do Cliente
 class Pergunta(BaseModel):
     msg: str
 
 #Criando um endpoint para teste
 @app.post('/ai')
-def chat(pergunta: Pergunta):
-    
-    #Gerando embedding dos livros
-    livros = lvr.buscar_todos_livros()
-    text = [livro['lvr_titulo'] + " " + livro['lvr_sinopse'] for livro in livros]
-    
-    books_embedding = ebd.gerar_embedding(text)
-    
-    #Gerando index faiss com base no embedding do livros
-    index = fid.criar_index_faiss(books_embedding)
+def chatbot(pergunta: Pergunta):
     
     #Gerando embedding da pergunta do usuário
     query_embedding = ebd.gerar_embedding([pergunta.msg])
@@ -51,36 +71,12 @@ def chat(pergunta: Pergunta):
     _, indices = index.search(query_embedding, k)
 
     #Montando um contexto para a IA
-    context = ''
+    context = 'Dados a serem considerados na conversa: \n\n'
     for i in indices[0]:
         livro = livros[i]
         context += f'Título: {livro['lvr_titulo']}\nDescrição: {livro['lvr_sinopse']}\n\nPreço {livro['lvr_preco']}\n'
     
-    
-    #Preparando a intrução para a IA
-    system_instruction = (
-        "Você é um assistente pessoal de um e-commerce de livros chamado weblibrary e deve agir como tal, respondendo perguntas relacionadas a livros. Sue nome não é mais Gemini, você é IA do e-commerce WebLibrary. Além disso você deve agir de forma amígavel e persuasiva ao mesmo tempo, a fim de fazer com que o vendedor se interesse em comprar determinado tipo de livro \n------------------------------------\nUse as informações dos livros abaixo para responder às perguntas de forma concisa:\n\n" + context
-    )
-   
-    #INICIALIZANDO A IA
-   
-    #Obtendo a api_key
-    load_dotenv()
-    api_key = os.getenv('GOOGLE_API_KEY')
-    
-    #Configurando a minha api_key
-    client = genai.Client(api_key=api_key)
-    
-    #Defininfo o meumodelo e o conteúdo que eu vou mostrar
-    model = 'gemini-2.0-flash'
-    
-    #Configurando o conteúdo que deve ser gerado
-    chat_config = types.GenerateContentConfig(
-        system_instruction = system_instruction
-    )
-    
-    #Criando um chat para conversar com a IA
-    chat = client.chats.create(model=model, config=chat_config)
+    line = '-------------------------------'
     
     #Retornando a resposta da IA
-    return {'ai_res': chat.send_message(pergunta.msg).text}
+    return {'ai_res': chat.send_message(f'{pergunta.msg}\n{line}\n{context}').text}
