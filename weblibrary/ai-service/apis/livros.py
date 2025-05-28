b1295de42bb21caf605922a0ca30dc08cd902e7a
joao_s_neto <joao72neto@gmail.com>
2025-05-28 16:31:14 -0300

    feat: chamar os dados da API de livros com o python

--- weblibrary/ai-service/apis/livros.py
+++ weblibrary/ai-service/apis/livros.py
@@ -1,1 +1,21 @@
-import requests
+import requests as req
+
+#Criando cache para todos os livros
+livros_cache = []
+
+def buscar_todos_livros(url='http://localhost:3000/api/books'):
+    
+    global livros_cache;
+    if not livros_cache:
+    
+        #Obtendo a resposta da API
+        res = req.get(url)
+        
+        #Verificando a requisição
+        if not res.status_code == 200:
+            print(f'Não foi possível obter os dados da API: {res.status_code}')
+            return;
+        
+        livros_cache = res.json()
+        
+    return livros_cache
