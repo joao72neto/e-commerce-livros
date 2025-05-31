fca769d0dc2eda12bf78e874e1e5a0c379af17f2
joao_s_neto <joao72neto@gmail.com>
2025-05-31 17:04:56 -0300

    feat: criar função que mostra os pedidos do cliente logado de forma organizada para a IA

--- weblibrary/ai-service/apis/pedidos.py
+++ weblibrary/ai-service/apis/pedidos.py
@@ -5,7 +5,7 @@ from clientes import clt_id_logado
 clt_logado = clt_id_logado()
 
 #Função que busca os pedidos do cliente logado
-def pedidos_clt_logado(url=f'http://localhost:3000/api/pedidos/{clt_logado}'):
+def buscar_pedidos_clt_logado(url=f'http://localhost:3000/api/pedidos/{clt_logado}'):
     
     #Obtendo os pedidos do cliente logado
     res = req.get(url)
@@ -17,3 +17,25 @@ def pedidos_clt_logado(url=f'http://localhost:3000/api/pedidos/{clt_logado}'):
     pedidos = res.json()
     return pedidos
     
+
+#Função que retorna os pedidos organizados para a IA
+def pedidos_contexto():
+    
+    context = ''
+    pedidos = buscar_pedidos_clt_logado()
+    for pedido in pedidos:
+    
+        context  += (
+            f'Nome do livro: {pedido['lvr_titulo']}\n'
+            f'Número do Pedido: {pedido['vnd_numPedido']}\n'
+            f'Data do Pedido: {pedido['vnd_data']}\n'
+            f'Status do Pedido: {pedido['vnd_status']}\n'
+            f'Valor Total: {pedido['vnd_valorTotal']}\n'
+            f'Valo do Frete: {pedido['vnd_frete']}\n'
+            f'Quantidade Comprada: {pedido['vnd_qtd']}\n'
+            f'Quantidade Trocada: {pedido['vnd_qtd_trocada']}\n\n' 
+        )
+        
+    return str(context)
+
+
