27fdcfa11368126e87beb648bbee3f87e84cfe2a
joao_s_neto <joao72neto@gmail.com>
2025-06-02 09:56:41 -0300

    feat: adicionar função python que tras todos os cartões de um cliente

--- weblibrary/ai-service/apis/cartoes.py
+++ weblibrary/ai-service/apis/cartoes.py
@@ -1,1 +1,19 @@
-import requests as red
+import requests as req
+from clientes import clt_id_logado
+
+def buscar_cartoes_clt_logado():
+    
+    #Monstando a url
+    clt_id = clt_id_logado()
+    url = f'http://localhost:3000/api/card/clt_id/{clt_id}'
+    
+    #Obtendo a respota do servidos
+    res = req.get(url)
+    
+    if not res.status_code == 200:
+        print(f'Não foi possível obter os cartões do cliente: {res.status_code}')
+        return
+    
+    #Pegando os dados em json
+    cartoes = res.json()
+    return cartoes
