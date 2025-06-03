7d56fdf8353f430e0f7bae9171b85d707e1e41f6
João Neto <joao72neto@gmail.com>
2025-06-03 16:14:50 -0300

    refactor: retirar arquivos e comentários desnecessários

--- weblibrary/ai-service/build/embeddings.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-import numpy as np
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def gerar_embedding(frases: list[str]):
-    query_embedding = model.encode(frases, normalize_embeddings=True)
-    query_embedding = np.array(query_embedding).astype('float32')
-    return query_embedding
