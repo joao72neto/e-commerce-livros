a421bf0a49479007cf0defd1a6b70f52f364b150
joao_s_neto <joao72neto@gmail.com>
2025-06-06 19:55:12 -0300

    chore: adicionar arquivos e dependências para a análise do histórico de vendas

---
+++ weblibrary/frontend/public/javaScript/pages/analise/historicoVendas.js
@@ -0,0 +1,10 @@
+//Carregando todas as funções relacionadas ao histórico de vendas
+document.addEventListener('DOMContentLoaded', function(){
+
+    teste()
+});
+
+
+function teste(){
+    console.log('Ola');
+}
