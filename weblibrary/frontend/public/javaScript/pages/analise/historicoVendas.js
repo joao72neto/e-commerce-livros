64f91dcf9605ebce76e96ba658f041bfc82d05f7
joao_s_neto <joao72neto@gmail.com>
2025-06-06 21:32:57 -0300

    feat: adicionar um gráfico real na tela de análide de histórico de vendas

--- weblibrary/frontend/public/javaScript/pages/analise/historicoVendas.js
+++ weblibrary/frontend/public/javaScript/pages/analise/historicoVendas.js
@@ -1,10 +1,57 @@
-//Carregando todas as funções relacionadas ao histórico de vendas
 document.addEventListener('DOMContentLoaded', function(){
 
-    teste()
+    //Carregando todas as funções relacionadas ao histórico de vendas
+    montarGrafico();
+
 });
 
-
-function teste(){
-    console.log('Ola');
+function montarGrafico() {
+    const ctx = document.getElementById('historico-vendas');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Jan', 'Fev', 'Mar', 'Abr'],
+            datasets: [
+                {
+                    label: 'Produto X',
+                    data: [10, 15, 10, 11],
+                    borderColor: 'red',
+                    borderWidth: 2,
+                    tension: 0.3
+                },
+                {
+                    label: 'Produto Y',
+                    data: [6, 5, 40, 2],
+                    borderColor: 'blue',
+                    borderWidth: 2,
+                    tension: 0.3
+                },
+                {
+                    label: 'Produto Z',
+                    data: [20, 6, 1, 34],
+                    borderColor: 'green',
+                    borderWidth: 2,
+                    tension: 0.3
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            interaction: {
+                intersect: false,
+                mode: 'index'
+            },
+            plugins: {
+                legend: {
+                    position: 'top',
+                    onClick: null,
+                    labels: {
+                        usePointStyle: true,
+                        pointStyle: 'circle',
+                        padding: 20
+                    }
+                }
+            }
+        }
+    });
 }
