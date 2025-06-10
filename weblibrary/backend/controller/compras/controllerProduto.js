fd497d21359c09a669546f4554d2b1393c393b9c
joao_s_neto <joao72neto@gmail.com>
2025-06-10 15:00:59 -0300

    fix: não mostrar IA na página de produtos para usuários não logados

--- weblibrary/backend/controller/compras/controllerProduto.js
+++ weblibrary/backend/controller/compras/controllerProduto.js
@@ -1,9 +1,13 @@
 const { buscarLivroId } = require('../../model/books/modelBooks');
+const {buscarClienteLogado } = require('../../model/clientes/modelClientes'); 
 
 //Página
 module.exports.getProduto = async (req, res) => {
     const livro = await buscarLivroId(req.params.lvr_id);
+    const clienteLogado = await buscarClienteLogado();
+
     return res.render('compras/produto', {
-        livro: livro[0]
+        livro: livro[0],
+        cliente: clienteLogado
     });
 };
