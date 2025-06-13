501b195e025f5b3393f892a829a07fc37959f830
joao_s_neto <joao72neto@gmail.com>
2025-06-13 17:00:13 -0300

    feat: mostrar os dados reais no gráfico da página de análise

--- weblibrary/frontend/public/javaScript/pages/analise/historicoVendas.js
+++ weblibrary/frontend/public/javaScript/pages/analise/historicoVendas.js
@@ -1,3 +1,5 @@
+import { buscarLivrosVendidoService } from "/javaScript/service/analise/serviceHistoricoVendas.js";
+
 document.addEventListener('DOMContentLoaded', function(){
 
     //Carregando todas as funções relacionadas ao histórico de vendas
@@ -26,38 +28,52 @@ function montarSelectCategorias(){
     });
 }
 
-function montarGrafico() {
+async function montarGrafico() {
     const ctx = document.getElementById('historico-vendas');
+    const livros_analise = await buscarLivrosVendidoService();
+
+    //Agrupando as datas
+    const datas = [...new Set(livros_analise.map(item => item.data_venda))].sort();
+    console.log(datas);
+
+    //Agrupando os livros com as datas
+    const livrosMap = {};
+    livros_analise.forEach(item => {
+        
+        //Obtendo dados
+        const titulo = item.lvr_titulo;
+        const data = item.data_venda;
+        const qtd = parseInt(item.total_vendido);
+
+        if(!livrosMap[titulo]){
+            livrosMap[titulo] = {};
+        }
+        
+        livrosMap[titulo][data] = qtd;
+
+    });
+
+    //Montando o Dataset
+    const datasets = Object.entries(livrosMap).map(([titulo, vendasPorData]) => {
+        const data = datas.map(data => vendasPorData[data] || 0);
+        return {
+            label: titulo,
+            data: data,
+            borderColor: gerarCorAleatoria(),
+            borderWidth: 3,
+            tension: 0.3,
+            clip: false
+        }
+    });
+
     new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['Jan', 'Fev', 'Mar', 'Abr'],
-            datasets: [
-                {
-                    label: 'Produto X',
-                    data: [10, 15, 10, 11],
-                    borderColor: 'red',
-                    borderWidth: 2,
-                    tension: 0.3
-                },
-                {
-                    label: 'Produto Y',
-                    data: [6, 5, 40, 2],
-                    borderColor: 'blue',
-                    borderWidth: 2,
-                    tension: 0.3
-                },
-                {
-                    label: 'Produto Z',
-                    data: [20, 6, 1, 34],
-                    borderColor: 'green',
-                    borderWidth: 2,
-                    tension: 0.3
-                }
-            ]
+            labels: datas,
+            datasets: datasets
         },
         options: {
-            responsive: false,
+            responsive: true,
             interaction: {
                 intersect: false,
                 mode: 'index'
@@ -76,3 +92,11 @@ function montarGrafico() {
         }
     });
 }
+
+//Função que gera cores aleatórias
+function gerarCorAleatoria() {
+    const r = Math.floor(Math.random() * 156) + 100;
+    const g = Math.floor(Math.random() * 156) + 100;
+    const b = Math.floor(Math.random() * 156) + 100;
+    return `rgb(${r}, ${g}, ${b})`;
+  }
