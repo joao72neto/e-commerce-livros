b7623cff9d3391f6d491318df7021953f712e140
joao_s_neto <joao72neto@gmail.com>
2025-06-13 14:49:37 -0300

    chore(analise): adicionar funções para pegar as categorias e livros para análise

--- weblibrary/backend/model/analise/modelHistoricoVendas.js
+++ weblibrary/backend/model/analise/modelHistoricoVendas.js
@@ -2,25 +2,55 @@ const { getDb } = require('../../config/db');
 
 //SELECT
 
-//Obtendo a view com os dados de vendas
-module.exports.buscarViewHistoricoVendas = async () => {
+//Obtendo categorias distintas da vw_historico_vendas
+module.exports.buscarCategoriasVendidas = async () => {
     
     //Obtendo o banco
     const db = await getDb();
 
     sql = `
-        select 
-            *
-        from
+        select distinct 
+            cat_nome 
+        from 
             vw_historico_vendas;
     `;
 
     try{
-        const [historico_vendas] = await db.query(sql);
-        return historico_vendas;
+        const [categorias] = await db.query(sql);
+        return categorias;
         
     }catch(err){
-        console.error(`Erro no buscarViewHistoricoVendas - modelHistoricoVendas: ${err}`);
+        console.error(`Erro no buscarCategoriasVendidas - modelHistoricoVendas: ${err}`);
+        throw err;
+    }
+}
+
+//Obtendo os livros vendidos da vw_historico_vendas
+module.exports.buscarLivrosVendidos = async () => {
+    
+    //Obtendo o banco
+    const db = await getDb();
+
+    sql = `
+        select
+            lvr_titulo,
+            sum(vnd_qtd) total_vendido,
+            date(vnd_data) data_venda
+        from 
+            vw_historico_vendas
+        group by
+            lvr_titulo,
+            date(vnd_data)
+        order by
+            data_venda;
+    `;
+
+    try{
+        const [livros] = await db.query(sql);
+        return livros;
+        
+    }catch(err){
+        console.error(`Erro no buscarLivrosVendidos - modelHistoricoVendas: ${err}`);
         throw err;
     }
 }
