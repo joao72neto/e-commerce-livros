dc8fe106fdab9d7dc9dc2c9be0e56102356f7ddf
joao_s_neto <joao72neto@gmail.com>
2025-06-13 14:54:14 -0300

    chore(controller): passar dados da análise de vendas para o endpoint da página

--- weblibrary/backend/controller/analise/controllerHistoricoVendas.js
+++ weblibrary/backend/controller/analise/controllerHistoricoVendas.js
@@ -1,4 +1,16 @@
+const { buscarCategoriasVendidas } = require('../../model/analise/modelHistoricoVendas');
+const { buscarLivrosVendidos } = require('../../model/analise/modelHistoricoVendas');
+
 //Página
-module.exports.getHistoricoVendas = (req, res) => {
-    return res.render('analise/historicoVendas');
+module.exports.getHistoricoVendas = async (req, res) => {
+    
+    //Obtendo os dados
+    const livros = await buscarLivrosVendidos();
+    const categorias = await buscarCategoriasVendidas();
+    
+    //Renderizando a página
+    return res.render('analise/historicoVendas', {
+        categorias: categorias,
+        livros: livros
+    });
 };
