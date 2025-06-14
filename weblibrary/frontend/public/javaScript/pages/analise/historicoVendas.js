60095a0d5cd99b8d8e24dbaaa1592942a92f556e
joao_s_neto <joao72neto@gmail.com>
2025-06-13 21:16:01 -0300

    feat(analise): mostrar os dados na tela de análise com o ApexCharts

--- weblibrary/frontend/public/javaScript/pages/analise/historicoVendas.js
+++ weblibrary/frontend/public/javaScript/pages/analise/historicoVendas.js
@@ -4,20 +4,20 @@ document.addEventListener('DOMContentLoaded', function(){
 
     //Carregando todas as funções relacionadas ao histórico de vendas
     montarGrafico();
-    montarSelectLivros();
+    // montarSelectLivros();
     montarSelectCategorias();
 
 });
 
-function montarSelectLivros(){
-    const livros = document.querySelector('#livros-filtro');
-    new Choices(livros, {
-        removeItemButton: true,  
-        placeholderValue: 'Pesquisar...',          
-        maxItemCount: 10 
-    });
+// function montarSelectLivros(){
+//     const livros = document.querySelector('#livros-filtro');
+//     new Choices(livros, {
+//         removeItemButton: true,  
+//         placeholderValue: 'Pesquisar...',          
+//         maxItemCount: 10 
+//     });
    
-}
+// }
 
 function montarSelectCategorias(){
     const categorias = document.querySelector('#categorias-filtro');
@@ -30,54 +30,47 @@ function montarSelectCategorias(){
 
 async function montarGrafico() {
     
-    // //Obtendo os dados para análise
-    // const livros_analise = await buscarLivrosVendidoService();
+    //Obtendo os dados para análise
+    const livros_analise = await buscarLivrosVendidoService();
 
-    // //Agrupando as datas
-    // const datas = [...new Set(livros_analise.map(item => item.data_venda))].sort();
-    // console.log(datas);
+    //Agrupando as datas
+    const datas = [...new Set(livros_analise.map(item => item.data_venda))].sort();
+    console.log(datas);
 
-    // //Agrupando os livros com as datas
-    // const livrosMap = {};
-    // livros_analise.forEach(item => {
+    //Agrupando os livros com as datas
+    const livrosMap = {};
+    livros_analise.forEach(item => {
         
-    //     //Obtendo dados
-    //     const titulo = item.lvr_titulo;
-    //     const data = item.data_venda;
-    //     const qtd = parseInt(item.total_vendido);
+        //Obtendo dados
+        const titulo = item.lvr_titulo;
+        const data = item.data_venda;
+        const qtd = parseInt(item.total_vendido);
 
-    //     if(!livrosMap[titulo]){
-    //         livrosMap[titulo] = {};
-    //     }
+        if(!livrosMap[titulo]){
+            livrosMap[titulo] = {};
+        }
         
-    //     livrosMap[titulo][data] = qtd;
+        livrosMap[titulo][data] = qtd;
 
-    // });
+    });
 
-    // //Montando o Dataset
-    // const datasets = Object.entries(livrosMap).map(([titulo, vendasPorData]) => {
-    //     const data = datas.map(data => vendasPorData[data] || 0);
-    //     return {
-    //         label: titulo,
-    //         data: data,
-    //         borderColor: gerarCorAleatoria(),
-    //         borderWidth: 3,
-    //         tension: 0.3,
-    //         clip: false
-    //     }
-    // });
+    //Montando o Dataset
+    const datasets = Object.entries(livrosMap).map(([titulo, vendasPorData]) => {
+        const data = datas.map(data => vendasPorData[data] || 0);
+        return {
+            name: titulo,
+            data: data,
+        }
+    });
 
     //Configurando o gráfico
     const options = {
         chart: {
             type: 'line'
         },
-        series: [{
-            name: 'sales',
-            data: [30,40,35,50,49,60,70,91,125]
-        }],
+        series: datasets,
         xaxis: {
-            categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+            categories: datas
         }
     }
 
