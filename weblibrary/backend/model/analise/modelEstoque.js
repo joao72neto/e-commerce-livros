b4636578a79716586c09cb449152a0e6463a6875
joao_s_neto <joao72neto@gmail.com>
2025-06-18 14:39:40 -0300

    refactor(model): obter os dados do estoque a partir da view vw_estoque

--- weblibrary/backend/model/analise/modelEstoque.js
+++ weblibrary/backend/model/analise/modelEstoque.js
@@ -54,28 +54,13 @@ module.exports.buscarEstoque = async () => {
     //Obtendo o banco
     const db = await getDb();
 
+    //Buscando a view do estoque
     const sql = `
     
         select
-            e.est_id,
-            f.for_id,
-            f.for_nome,
-            l.lvr_id,
-            l.lvr_titulo,
-            g.gpp_id,
-            g.gpp_nome,
-            g.gpp_margemLucro,
-            e.est_qtd,
-            e.est_data,
-            e.est_valorCompra,
-            e.est_origem,
-            round((((g.gpp_margemLucro / 100) + 1) * e.est_valorCompra), 2) valorVenda
+            *
         from 
-            estoque e
-            join grupo_precificacao g on g.gpp_id = e.est_gpp_id
-            join fornecedor f on f.for_id = e.est_for_id
-            join livros l on l.lvr_id = e.est_lvr_id
-        order by e.est_id desc;
+            vw_estoque;
     `;
     
     try{
