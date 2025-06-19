a41cb988c195f8d687220a3f229fec7596abd3ab
joao_s_neto <joao72neto@gmail.com>
2025-06-19 19:49:05 -0300

    chore(controller): passar a qtd de livros disponíveis no estoque para a página de produto

--- weblibrary/backend/controller/compras/controllerProduto.js
+++ weblibrary/backend/controller/compras/controllerProduto.js
@@ -1,15 +1,18 @@
 const { buscarLivroId } = require('../../model/books/modelBooks');
 const { buscarCategoriasLivroId } = require('../../model/books/modelBooks');
 const { buscarClienteLogado } = require('../../model/clientes/modelClientes'); 
+const { buscarLivrosIdQtdEstoque } = require('../../model/analise/modelEstoque');
 
 //Página
 module.exports.getProduto = async (req, res) => {
     const livro = await buscarLivroId(req.params.lvr_id);
+    const livroQtdEstoque = await buscarLivrosIdQtdEstoque(req.params.lvr_id);
     const clienteLogado = await buscarClienteLogado();
     const categorias = await buscarCategoriasLivroId(req.params.lvr_id);
 
     return res.render('compras/produto', {
         livro: livro[0],
+        livroQtdEstoque: livroQtdEstoque,
         cliente: clienteLogado,
         categorias: categorias
     });
