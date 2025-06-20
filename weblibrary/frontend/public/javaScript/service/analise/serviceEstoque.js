1d70053717b0929b50eaff737a7278ff6080f3d6
joao_s_neto <joao72neto@gmail.com>
2025-06-19 23:03:32 -0300

    chore(service): adicionar função que chama a API para atualizar o estoque

--- weblibrary/frontend/public/javaScript/service/analise/serviceEstoque.js
+++ weblibrary/frontend/public/javaScript/service/analise/serviceEstoque.js
@@ -24,3 +24,22 @@ export async function adicionarEstoqueService(dados) {
         throw err;
     }
 }
+
+//PATCH
+
+//Atualizando o estoque
+export async function atualizarEstoqueService(dados) {
+    try{
+        let res = await fetch(`/estoque/update`, {
+            method: 'PATCH',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(dados)
+        });
+        
+        return res.status;
+
+    }catch(err){
+        console.error(`Erro no atualizarEstoqueService - serviceEstoque: ${err}`);
+        return 500;
+    }
+}
