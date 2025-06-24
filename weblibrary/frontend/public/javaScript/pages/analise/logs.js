8469a7bc4e01ed4981caa35f8ada721ee665f1c9
joao_s_neto <joao72neto@gmail.com>
2025-06-24 15:39:45 -0300

    feat/style(logs): add a msg when there are no logs registered

--- weblibrary/frontend/public/javaScript/pages/analise/logs.js
+++ weblibrary/frontend/public/javaScript/pages/analise/logs.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){
-    console.log('Hello');
+    emptyLogMsg();
 });
+
+function emptyLogMsg(){
+
+    //Getting the necessary elements
+    const tBody = document.querySelector('.tb-body');
+    const container = document.querySelector('.container');
+
+    //Returning in case of registered logs
+    if(tBody) return;
+
+    //Creating a personalized msg in case of no logs
+    const html = `
+        <div class="empty">
+            <p>
+                Nenhum Log Registrado
+            </p>
+        </div>
+    `;
+
+    //Injecting HTML code into the log page
+    container.innerHTML = html;
+}
