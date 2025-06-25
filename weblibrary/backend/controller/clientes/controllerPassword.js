b1c2165ba04d79c0b4349bba4e0b6380c1cf7e6d
joao_s_neto <joao72neto@gmail.com>
2025-06-25 19:43:54 -0300

    feat(logs): register log when changing user password

--- weblibrary/backend/controller/clientes/controllerPassword.js
+++ weblibrary/backend/controller/clientes/controllerPassword.js
@@ -1,4 +1,5 @@
 const { alterarSenhaCliente, buscarClienteId } = require("../../model/clientes/modelClientes");
+const { registerLog } = require('../../model/analise/modelLogs');
 const bcrypt = require('bcryptjs');
 
 //Páginas
@@ -6,11 +7,30 @@ module.exports.getPassword = (req, res) => {
     return res.render('clientes/password');
 };
 
+//Log model
+let logData = {
+    log_clt_id: '',
+    log_usuario: '',
+    log_operacao: '',
+    log_desc: ''
+}
+
 //Alterando senha
 module.exports.patchPassword =  async (req, res) => {
     try{
+
         await alterarSenhaCliente(req.body, req.params.clt_id);
+
+        //Registering log
+        const client = await buscarClienteId(req.params.clt_id);
+        const userName = client[0].clt_nome;
+        logData.log_usuario = '(Admin) ';
+        logData.log_operacao = 'UPDATE';
+        logData.log_desc = `Senha de "${userName}" alterada`;
+        await registerLog(logData);
+
         return res.sendStatus(204);
+
     }catch(err){
         console.error(`Erro no patchPassword - controllerPassword ${err}`);
         return res.sendStatus(500);
