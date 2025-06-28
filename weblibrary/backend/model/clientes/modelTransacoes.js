1774e3c9b2eec2b490a71b69c6f6d0927720914f
joao_s_neto <joao72neto@gmail.com>
2025-06-27 22:00:34 -0300

    style(transactions): adjust transaction page style

--- weblibrary/backend/model/clientes/modelTransacoes.js
+++ weblibrary/backend/model/clientes/modelTransacoes.js
@@ -24,8 +24,25 @@ module.exports.buscarTransacoesClienteId = async (id) => {
     //Obtendo o banco
     const db = await getDb();
 
+    const sql = `
+        select
+            *
+        from
+            transacoes
+        where
+            trs_clt_id = ?
+        order by
+            trs_dataHora desc;
+    `;
+
     try{
-        const [transacoes] = await db.query(`select * from transacoes where trs_clt_id = ?`, id);
+        let [transacoes] = await db.query(sql, id);
+
+        transacoes = transacoes.map(trs => ({
+            ...trs,
+            trs_dataHora: new Date(trs.trs_dataHora).toLocaleString('pt-BR')
+        }))
+
         return transacoes;
         
     }catch(err){
