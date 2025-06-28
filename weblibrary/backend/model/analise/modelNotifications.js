216489e942fef39876dcc6530123e69847f64e2f
joao_s_neto <joao72neto@gmail.com>
2025-06-28 20:15:37 -0300

    chore(model): create file for notification operatios

---
+++ weblibrary/backend/model/analise/modelNotifications.js
@@ -0,0 +1,39 @@
+const { getDb } = require('../../config/db');
+const { buscarClienteLogado } = require('../../model/clientes/modelClientes');
+
+//INSERT
+
+//Registering notifications
+module.exports.registerNotification = async (dados) => {
+
+    //Setting up db
+    const db = await getDb();
+
+    //Getting the logged in user
+    const client = await buscarClienteLogado();
+    dados.log_clt_id = client[0].clt_id;
+
+    //Preparing sql query
+    const sql = `
+        INSERT INTO notifications (
+            not_clt_id,
+            not_datetime,
+            not_title,
+            not_msg,
+            not_status
+        ) VALUES (
+            ?, NOW(), ?, ?, ?
+        );
+    `;
+
+    //Preparing values to be inserted
+    const valores = Object.values(dados);
+
+    //Inserting the data into the db
+    try{
+        await db.query(sql, valores);
+    }catch(err){
+        console.error(`Erro no registerNotification - modelNotifications: ${err}`);
+        throw err;
+    }
+}
